dba4d8d9bcc6c5bab0fcd4d14f4c2effa8dc223a
Pavan <pavanmishra@gmail.com>
2014-10-02 03:20:56 +0530

    preparing setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name="less.web.py",
+    version="0.1.0",
+    author="Pavan Mishra",
+    author_email="pavanmishra@gmail.com",
+    packages=["less"],
+    include_package_data=True,
+    url="http://pypi.python.org/pypi/less.web.py_v010/",
+    license="LICENSE",
+    description="Masking the web.py magic",
+	long_description=open("README.md").read(),
+    install_requires=[
+        "web.py",
+    ],
+)
